559a7081f72b41bb16449c8bd6a2a71a8c3b23e3
Vadim Mostovenko <mostovenko.vadim@wb.ru>
2024-05-29 23:52:15 +0300

    add review unit test

--- src/review/review.service.spec.ts
+++ src/review/review.service.spec.ts
@@ -1,18 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReviewService } from './review.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+import { ReviewModel } from './review.model';
 
 describe('ReviewService', () => {
 	let service: ReviewService;
 
+	const exec = { exec: jest.fn() };
+	const reviewRepositoryFactory = () => ({
+		find: () => exec,
+	});
+
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [ReviewService],
+			providers: [
+				ReviewService,
+				{ useFactory: reviewRepositoryFactory, provide: getModelToken(ReviewModel.name) },
+			],
 		}).compile();
 
 		service = module.get<ReviewService>(ReviewService);
 	});
 
-	it('should be defined', () => {
-		expect(service).toBeDefined();
+	it('findByProductId - success', async () => {
+		const id = new Types.ObjectId().toHexString();
+
+		reviewRepositoryFactory()
+			.find()
+			.exec.mockResolvedValueOnce([{ productId: id }]);
+
+		const res = await service.findByProductId(id);
+		expect(res[0].productId).toBe(id);
 	});
 });

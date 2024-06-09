9085645683c5b224bb898787d66d4cd2c940f51e
Vadim Mostovenko <mostovenko.vadim@wb.ru>
2024-06-09 20:36:13 +0300

    add jwt login logic

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,20 @@ import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModel, UserSchema } from './user.model';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getJWTConfig } from 'src/configs/jwt.config';
 
 @Module({
 	controllers: [AuthController],
-	imports: [MongooseModule.forFeature([{ name: UserModel.name, schema: UserSchema }])],
+	imports: [
+		MongooseModule.forFeature([{ name: UserModel.name, schema: UserSchema }]),
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJWTConfig,
+		}),
+	],
 	providers: [AuthService],
 })
 export class AuthModule {}
